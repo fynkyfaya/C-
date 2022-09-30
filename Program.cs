bf4d5e43b1b6686a9267c58e5d510ad9aaa559c1
Фадеев Тимур Сергеевич <110234293+fynkyfaya@users.noreply.github.com>
2022-09-30 21:02:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите значение R:");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение U:");
+double y = double.Parse(Console.ReadLine());
+double res = (x * y);
+Console.WriteLine($"{res:F2}");
